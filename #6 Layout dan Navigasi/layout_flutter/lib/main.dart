562e2c878f70011c662a268385106c74df2101e7
niky tania <nikytania0701@gmail.com>
2026-04-10 22:29:21 +0700

    Membangun Layout di Flutter

--- #6 Layout dan Navigasi/layout_flutter/lib/main.dart
+++ #6 Layout dan Navigasi/layout_flutter/lib/main.dart
@@ -7,15 +7,42 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /* soal 1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /* soal 2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Wisata Gunung di Batu',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Text(
+                  'Batu, Malang, Indonesia',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+          /* soal 3*/
+          Icon(Icons.star, color: Colors.red),
+          const Text('41'),
+        ],
+      ),
+    );
+
     return MaterialApp(
       title: 'Flutter layout: Niky Tania Sari dan 244107060053',
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter layout demo'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+        appBar: AppBar(title: const Text('Flutter layout demo')),
+        body: titleSection,
       ),
     );
   }
