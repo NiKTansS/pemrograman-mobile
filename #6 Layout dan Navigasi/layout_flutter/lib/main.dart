4df77fe599e80f141e664e76392929131877000d
niky tania <nikytania0701@gmail.com>
2026-04-11 07:07:22 +0700

    Implementasi text section

--- #6 Layout dan Navigasi/layout_flutter/lib/main.dart
+++ #6 Layout dan Navigasi/layout_flutter/lib/main.dart
@@ -49,11 +49,29 @@ class MyApp extends StatelessWidget {
       ],
     );
 
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: const Text(
+        'Carilah teks di internet yang sesuai '
+        'dengan foto atau tempat wisata yang ingin '
+        'Anda tampilkan. '
+        'Tambahkan nama dan NIM Anda sebagai '
+        'identitas hasil pekerjaan Anda. '
+        'Selamat mengerjakan 🙂.',
+        softWrap: true,
+      ),
+    );
+
     return MaterialApp(
       title: 'Flutter layout: Niky Tania Sari dan 244107060053',
       home: Scaffold(
         appBar: AppBar(title: const Text('Flutter layout demo')),
-        body: Column(children: [titleSection, buttonSection]),
+        body: Column(children: [
+          titleSection, 
+          buttonSection,
+          textSection,
+          ]
+        ),
       ),
     );
   }
