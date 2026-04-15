b9667d0e2ceaff945f9e73db446fc7d5e8036be6
niky tania <nikytania0701@gmail.com>
2026-04-15 11:58:42 +0700

    update

---
+++ #6 Layout dan Navigasi/belanja/lib/pages/item_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import '../models/item.dart';
+
+class ItemPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Mengambil data yang dikirim dari HomePage
+    final itemArgs = ModalRoute.of(context)!.settings.arguments as Item;
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Item Detail')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Nama Barang: ${itemArgs.name}'),
+            Text('Harga: ${itemArgs.price}'),
+          ],
+        ),
+      ),
+    );
+  }
+}
