7ac3aca23da6b0cc332cc87c2df8d6518cb73690
niky tania <nikytania0701@gmail.com>
2026-04-21 10:44:48 +0700

    update details

--- #6 Layout dan Navigasi/belanja/lib/pages/home_page.dart
+++ #6 Layout dan Navigasi/belanja/lib/pages/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import '../models/item.dart';
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatelessWidget {
   HomePage({super.key});
   final List<Item> items = [
     Item(name: 'Sugar', price: 5000),
@@ -11,9 +11,7 @@ class HomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Shopping List'),
-      ),
+      appBar: AppBar(title: const Text('Shopping List')),
       body: Container(
         margin: EdgeInsets.all(8),
         child: ListView.builder(
@@ -24,7 +22,7 @@ class HomePage extends StatelessWidget{
 
             return InkWell(
               onTap: () {
-                Navigator.pushNamed(context, '/item');
+                Navigator.pushNamed(context, '/item', arguments: item);
               },
               child: Card(
                 child: Container(
@@ -36,15 +34,16 @@ class HomePage extends StatelessWidget{
                         child: Text(
                           item.price.toString(),
                           textAlign: TextAlign.end,
-                      )),
+                        ),
+                      ),
                     ],
                   ),
                 ),
               ),
             );
           },
-        )
-      )
+        ),
+      ),
     );
   }
 }
